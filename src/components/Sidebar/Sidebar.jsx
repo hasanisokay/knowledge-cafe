f89024911211342f1a244f176edb429865ab9869
hasanisokay <hasanisokay@gmail.com>
2023-03-31 01:11:41 +0600

    added sidebar styles to show data

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,10 +4,10 @@ import SingleBlog from '../SingleBlog/SingleBlog';
 const Sidebar = ({bookmark, markRead}) => {
     return (
         <div className='px-2'>
-            <div className=''>
-                <p className='text-blue-400 text-xl rounded mb-4'>Spent Time On Read: {markRead} min</p>
+            <div className='bg-blue-100  border-indigo-600 border w-full rounded mb-4 p-2'>
+                <p className='text-indigo-600 font-bold text-xl rounded mb-4'>Spent Time On Read: {markRead} min</p>
             </div>
-            <div className='flex flex-col bg-slate-200 rounded '>
+            <div className='flex flex-col bg-slate-200 rounded p-3 '>
                 <p className='text-xl font-bold'>Bookmarked Blogs: {bookmark.length}</p>
                 {
                    bookmark.map(blogName => <SingleBlog singleBlog={blogName}></SingleBlog>)
