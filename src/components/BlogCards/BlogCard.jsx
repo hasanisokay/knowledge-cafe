5f253eb02ada333b020d144568295cdc84f0a19a
hasanisokay <hasanisokay@gmail.com>
2023-03-30 23:10:58 +0600

    added json fetching

--- src/components/BlogCards/BlogCard.jsx
+++ src/components/BlogCards/BlogCard.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const BlogCard = () => {
+    const [blog, setBlog] = useState([])
+    useEffect(()=>{
+       fetch("api.json")
+       .then(res=>res.json())
+       .then(data=>console.log(data))
+    },[])
+
     return (
         <div className='lg:flex mx-4 my-4 gap-4'>
             <div className='blogs bg-slate-400 w-full '>
-            <h1>h</h1>
-            </div>
-            <div className='lg:sticky bg-success w-full  basis-2/12'>
+                {/* blog part */}
                 <h1>hi</h1>
             </div>
-            
+            <div className='lg:sticky bg-success w-full  basis-2/12'>
+                {/* sidebar */}
+                <h1>hi</h1>
+            </div>
+
         </div>
     );
 };
