0891653178add043508dc57059b137c688c2deae
hasanisokay <hasanisokay@gmail.com>
2023-03-31 00:22:59 +0600

    added handler function to buttons and fetching data from child component

--- src/components/BlogCards/BlogCard.jsx
+++ src/components/BlogCards/BlogCard.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import Sidebar from '../Sidebar/Sidebar';
 import SingleCard from '../SingleCard/SingleCard';
 
 const BlogCard = () => {
     const [blogs, setBlog] = useState([])
     const [bookmark, setBookmark]= useState("")
     const [markRead, setMarkRead]= useState("")
-    const handleMarkRead =()=>{
-
+    const handleMarkRead =(read_time)=>{
+        console.log(read_time);
     }
-    const handleBookmark =()=>{
+    const handleBookmark =(blog_title)=>{
 
     }
 
@@ -24,12 +25,12 @@ const BlogCard = () => {
         <div className='lg:flex md:flex lg:mx-12 lg:my-8 gap-4 mx-2 my-4'>
             <div className='blogs w-full flex flex-col gap-4 basis-5/6'>
                 {
-                    blogs.map(blog=> <SingleCard blog={blog} key={blog.id} ></SingleCard>)
+                    blogs.map(blog=> <SingleCard blog={blog} key={blog.id} bookmark={handleBookmark} markRead={handleMarkRead}></SingleCard>)
                 }
             </div>
             <div className='lg:sticky bg-success w-full  basis-1/4 h-96'>
                 {/* sidebar */}
-                <h1>hi</h1>
+                <Sidebar ></Sidebar>
             </div>
 
         </div>
--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const SingleCard = ({ blog }) => {
-    const { author_image, blog_title, read_time, publish_date, cover_image, author_name, tags } = blog;
+const SingleCard = (props) => {
+    console.log(props)
+    const { author_image, blog_title, read_time, publish_date, cover_image, author_name, tags } = props.blog;
     return (
         <div>
             <div className="card card-compact w-full  shadow-xl">
@@ -21,7 +22,7 @@ const SingleCard = ({ blog }) => {
                         </div>
                         <div className='flex'>
                             <p className='text-slate-500'>{read_time} min read</p>
-                            <span className="cursor-pointer" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <span className="cursor-pointer" onClick={()=>props.bookmark(blog_title)} ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                             </svg></span>
                         </div>
@@ -30,7 +31,7 @@ const SingleCard = ({ blog }) => {
                     <h2 className="card-title">{blog_title}</h2>
                     <p className='text-slate-500'>{tags.map(tag=>'#' + tag + " ")}</p>
                     <div className="card-actions">
-                        <button className='underline text-blue-600'>Mark as read</button>
+                        <button onClick={()=>props.markRead(read_time)} className='underline text-blue-600'>Mark as read</button>
                     </div>
                 </div>
                 
